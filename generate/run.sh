46be6a8e589bfbbbe28c447e30e6d25c685b29d1
Andreas Copan <avcopan@gmail.com>
2024-07-18 11:05:50 -0400

    Generates mechanism for 1-propyl radical

---
+++ generate/run.sh
@@ -0,0 +1,5 @@
+NUMBER=${1}
+
+cp build${NUMBER}.dat build.dat
+python ~/code/amech-dev/src/mechanalyzer/mechanalyzer_bin/build_mech.py
+
---
+++ stereoexpand/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python ~/code/amech-dev/src/mechanalyzer/mechanalyzer_bin/ste_mech.py
+
